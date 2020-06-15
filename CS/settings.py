c3858c47482c1fe5e7e8949035863d1017fdaf6b
leoanzures13 <leoanzures.la@gmail.com>
2020-06-15 14:25:05 -0500

    view integration 005

--- CS/settings.py
+++ CS/settings.py
@@ -141,9 +141,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-)
+]
 
 try:
     from CS.local_settings import*
