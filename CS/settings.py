befc7f8526a0ce43855ce34183761eef1c9c0b0b
leoanzures13 <leoanzures.la@gmail.com>
2020-06-15 01:43:57 -0500

    view integration 002

--- CS/settings.py
+++ CS/settings.py
@@ -60,6 +60,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #'whitenoise.middleware.WhiteNoiseMiddleware',
+]
+
+MIDDLEWARE_CLASSES = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
